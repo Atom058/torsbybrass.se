f5788723493deb138c09d09cbbdc3ab814c5f42a
Atom058 <anton.dahlstrom508@gmail.com>
2013-09-28 01:04:47 +0200

    Continued work with a bunch of files. Shouldn't commit like this...

--- header.php
+++ header.php
@@ -15,17 +15,20 @@
  	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
  	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
  	<link rel="stylesheet" href="<?php bloginfo( 'template_url' )?>/styles/layout.css" type="text/css" />
+ 	<link rel="stylesheet" href="<?php bloginfo( 'template_url' )?>/styles/colour.css" type="text/css" />
+ 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,400italic,700,300italic' rel='stylesheet' type='text/css'>
  	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
  	<?php wp_head(); ?>
  </head>
- <body <?php body_class(); ?>>
-	 	<header class="l-fullwidth">
+ <?php $classes = array('c-black', 'c-bg-white')?>
+ <body <?php body_class($classes); ?>>
+	 	<header class="l-fullwidth c-bg-black c-white">
 	 		<div id="header" class="l-pagewide header">
 	 			<h1>Torsbybrass.se!</h1>
  			</div>
  		</header>
-	 	<nav id="site-nav" class="main l-fullwidth" role="navigation">
+	 	<nav id="site-nav" class="main l-fullwidth c-bg-black c-white" role="navigation">
 	 		<?php wp_nav_menu( array( 
 	 			'menu' => '1'
 	 			, 'menu_class' => 'l-pagewide') ); ?>
--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php get_header(); ?>
 
-<div id="content" class="">
+<div id="content" class="l-pagewide">
 
 	<div class="">
 		<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-				<article id="post-<?php the_ID(); ?>">
-					<h1 class=""><a href="<?php the_permalink(); ?>" rel="bookmark" title="En permanent länk till artikeln '<?php the_title_attribute();?>'"><?php the_title(); ?></a></h1>
-					<div class="">
+				<article id="post-<?php the_ID(); ?>" class="l-margin-top-large">
+					<h1><a href="<?php the_permalink(); ?>" rel="bookmark" title="En permanent länk till artikeln '<?php the_title_attribute();?>'"><?php the_title(); ?></a></h1>
+					<div class="c-gray">
 						<?php the_date( 'F j, Y', 'Published on ', '', TRUE ) ?> by <?php echo(get_the_author()); ?>
 					</div>
-					<div class="content">
+					<div class="l-margin-top-small">
 							<?php the_content(); ?>
 					</div>
 					<div class="">
