aa0d83a8958e39689448ca73149427f11d4fd5e3
Atom058 <anton.dahlstrom508@gmail.com>
2013-12-29 18:06:18 +0100

    Change title display in sidebar

--- functions.php
+++ functions.php
@@ -7,6 +7,8 @@
 			, 'description' => 'The main sidebar of the site. Used on all pages by default.'
 			, 'before_widget' => '<section>'
 			, 'after_widget' => '</section>'
+			, 'before_title' => '<h2>'
+			, 'after_title' => '</h2>'
 		);
 	register_sidebar( $args );
 
