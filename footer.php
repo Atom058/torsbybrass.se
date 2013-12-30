c098199a7d0b964fcd6df0bd5afb5dc4479dfab3
atom058 <anton.dahlstrom508@gmail.com>
2013-12-31 00:00:46 +0100

    Updated footer with contact information

--- footer.php
+++ footer.php
@@ -3,8 +3,20 @@
 </div> <!-- page-content -->
 <div class="clearfix"></div>
 
-<footer> 
-  <p>Developed by Anton Dahlström - ©2013</p>
+<footer>
+	<p>
+		<span itemscope itemtype="http://schema.org/Organization">
+			<span itemprop="name">Torsby Brass</span>, 
+			<span itemprop="location" itemscope itemtype="http://schema.org/PostalAddress">
+				<span itemprop="streetAddress">Tingshusgatan 2</span>, 
+				<span itemprop="postalCode">685 30</span>
+				<span itemprop="AaddressLocality"> Torsby</span>
+			</span>, 
+			Tel: <span itemprop="telephone">0560-121 68</span>, 
+			Mob: <span itemprop="telephone">070-344 45 68</span>, 
+			E-post: <span itemprop="email">info@torsbybrass.se</span>
+		</span>
+	</p>
 </footer>
 
 </body>
