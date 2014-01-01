ddb3087350dc4c23ea4e23c9f8e79499955d95f0
atom058 <anton.dahlstrom508@gmail.com>
2014-01-01 17:47:41 +0100

    Logo added to header and style

--- header.php
+++ header.php
@@ -15,8 +15,6 @@
 	<title><?php wp_title(); ?></title>
 	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
-	<link rel="stylesheet" href="<?php bloginfo( 'template_url' )?>/styles/layout.css" type="text/css" />
-	<link rel="stylesheet" href="<?php bloginfo( 'template_url' )?>/styles/colour.css" type="text/css" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
 </head>
@@ -24,6 +22,7 @@
 <body <?php body_class($classes); ?>>
 	<header> 
 		<h1><a href="<?php echo get_home_url(); ?>" title="Till startsidan">Torsby Brass</a></h1>
+		<div class="header-logo"></div>
 	</header>
 	<nav>
 		<?php wp_nav_menu( array('menu' => '1') ); ?>
