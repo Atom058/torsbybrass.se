1d61258a1a174a41e826c7dbf51511897d1dd06c
atom058 <anton.dahlstrom508@gmail.com>
2014-01-02 17:36:44 +0100

    Added styling for plugins

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 	<title><?php wp_title(); ?></title>
 	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/style-plugins.css'; ?>" type="text/css" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
 </head>
