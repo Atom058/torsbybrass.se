96505c7d20803804cdd1da6e6aa75fb6351174f1
atom058 <anton.dahlstrom508@gmail.com>
2014-01-02 20:03:11 +0100

    Changed the way meta info is displayed

--- index.php
+++ index.php
@@ -10,15 +10,17 @@
 
 <?php get_header(); ?>
 
+<div class="posts">
 <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 	<article id="post-<?php the_ID(); ?>">
 		<h1><a href="<?php the_permalink(); ?>" rel="bookmark" title="En permanent länk till artikeln '<?php the_title_attribute();?>'"><?php the_title(); ?></a></h1>
 		<p class="meta">
-			<?php the_date( 'F j, Y', 'Published on ', '', TRUE ) ?> by <?php echo(get_the_author()); ?>
+			<?php the_date( 'j F, Y', 'Publicerat den ', '', TRUE ) ?> <?php if( get_the_author() !== admin ) echo( 'by' . get_the_author() ); ?>
 		</p>
 		<?php the_content(); ?>
 	</article>
 <?php endwhile; endif; ?>
+</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
