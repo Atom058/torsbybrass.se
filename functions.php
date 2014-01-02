144264b52105cebf384d5fc4deba2d194637ca96
atom058 <anton.dahlstrom508@gmail.com>
2014-01-02 22:36:30 +0100

    Removed unused image style

--- functions.php
+++ functions.php
@@ -12,24 +12,4 @@
 		);
 	register_sidebar( $args );
 
-
-	/*----- Adds image styles -----*/
-	if ( function_exists( 'add_theme_support' )	) {
-		add_theme_support( 'post-thumbnails' );
-		set_post_thumbnail_size( 150, 150 );//Default Post Thumbnail
-	
-		if ( function_exists( 'add_image_size' )	) {
-			add_image_size( 'post_large_image', 900, 9999 );
-			add_filter( 'image_size_names_choose', 'my_custom_sizes' );
-			
-			function my_custom_sizes( $sizes ) {
-				return array_merge( $sizes, array(
-					'post_large_image' => __('Stor bild')
-					)	
-				);
-			}
-		}
-		
-	}
-
 ?>
