611ea22eb3adefea101ebdd4d54e8f70f5e39e1a
atom058 <anton.dahlstrom508@gmail.com>
2014-01-11 15:14:47 +0100

    Removed manual versioningof stylesheets - now done by w3 cache

--- header.php
+++ header.php
@@ -14,8 +14,8 @@
 	<meta charset="<?php bloginfo( 'charset' )?>" />
 	<title><?php wp_title(); ?></title>
 	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
-	<link rel="stylesheet" href="<?php echo get_stylesheet_uri() . '?v=3'; ?>" type="text/css" />
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/style-plugins.css?v=3'; ?>" type="text/css" />
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/style-plugins.css'; ?>" type="text/css" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() . '/favicon.ico'; ?>" />
 	<?php wp_head(); ?>
